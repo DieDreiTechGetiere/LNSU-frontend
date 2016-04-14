4ff4a442d3bff0cc17522c8bbaabb5f0bdb9c929
Niklas Maier <n.maier@syzygy.de>
2016-04-14 11:32:15 +0200

    added release script

---
+++ release.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+
+echo ""
+echo "preparing release for $1"
+echo ""
+
+
+SCRIPT_DIRECTORY=$(cd "$(dirname "$0")"; pwd)
+
+#clear release dir
+#rm -r $SCRIPT_DIRECTORY/release/*
+
+#create new release folder
+RELEASE_FOLDER="$(date +"%D" | tr '[/]' ' ' | tr -d ' ')"_"$(date +"%T" | tr '[:]' '-')"
+mkdir $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER
+
+if [ "$1" == "all" ];then
+    #built finalize-built.js
+    cd htdocs
+    node r.js -o build.js
+    cd ..
+    
+    cp -r $SCRIPT_DIRECTORY/htdocs/app $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/app
+    cp -r $SCRIPT_DIRECTORY/htdocs/css $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/css
+    cp -r $SCRIPT_DIRECTORY/htdocs/media $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/media
+    
+elif [ "$1" == "js" ];then
+    #built finalize-built.js
+    cd htdocs
+    node r.js -o build.js
+    cd ..
+    
+    cp -r $SCRIPT_DIRECTORY/htdocs/app $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/app
+    
+elif [ "$1" == "css" ];then
+
+    cp -r $SCRIPT_DIRECTORY/htdocs/css $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/css
+    
+elif [ "$1" == "media" ];then
+
+    cp -r $SCRIPT_DIRECTORY/htdocs/media $SCRIPT_DIRECTORY/release/$RELEASE_FOLDER/media
+    
+fi
+
+duck --upload ftps://ud19_420@ud19.udmedia.de/html/lnsu-frontend /Users/nmaier/projects/my-projects/2016/LNSU-frontend/release
+
+
+exit 1;
