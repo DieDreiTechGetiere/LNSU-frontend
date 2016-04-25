e797cb9da734701d6f91e1ec9cf0b7f6457b9fc0
Niklas Maier <n.maier@syzygy.de>
2016-04-25 14:42:50 +0200

    Live Server Release 042516_14-42-11

--- htdocs/app/js/controller/ApplicationController.js
+++ htdocs/app/js/controller/ApplicationController.js
@@ -77,6 +77,7 @@ define(function(require)
         {
             if(app.matchModel != undefined)
             {
+                alert("close before");
                 app.execute(notification.command.match.DELETE);
             }
         },
